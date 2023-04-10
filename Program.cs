32ff5125bcff8b9ca80bb4cfcba0b591d62a9dd0
Ivan Solomennikov <van879@rambler.ru>
2023-04-11 00:20:11 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int enemyHealth = 10;
+			string mySlogan = "To beer, or not to beer?";
+			bool isDead = true;
+			float lie = 4.5f;
+			double pi = 3.14157;
+			byte age = 30;
+			ushort distance = 352;
+			char sector = 'F';
+			uint row = 4563;
+			ulong myMoney = 1234567890999;
+			
+		}
+	}
+}
